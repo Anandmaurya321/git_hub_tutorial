93eab19c58872a541e538c7d0c031a1e281772c1
Anand maurya <iliveinindia134@gmail.com>
2026-01-03 22:10:54 +0530

    all the files are added

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ app.get('/' , (req , res)=>{
     res.send.json({name:"Anand Maurya"})
 })
 
+app.get('/about' , (req ,res)=>{
+    console.log("res is running")
+    res.send.json({name:"Anand"});
+})
+
 app.listen(3000 , ()=>{
     console.log("running at  port no. 3000")
 })
+
+/**
+ * git init: initiate git :: but all are untracked till now
+ * git status : shows all status ::
+ * git add <file_name> : add for track ::
+ */
